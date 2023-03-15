6898f4bb70f36898a209a4a423b1b8d2505bb719
Avery Christie <averychristie50@gmail.com>
2023-03-15 15:06:33 -0600

    Added web discovery

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Enumeration-Script
-Project using bash scripting to (slightly) automate the process of enumerating a machine.
+Project using bash scripting to (slightly) automate the process of enumerating a machine. Just need to input the IP address of the machine to enumerate.
--- test_quick.sh
+++ test_quick.sh
@@ -9,13 +9,52 @@ count=$( grep "bytes from $IP_ADDR" -E ping_results.txt -c )
 pid=$$	# get process id of this script
 
 if [ $count -gt 0 ]	# -gt = greater than, -lt less than, -eq equal to
-then echo " "; echo "Found host"; echo "Starting a port scan"; echo " "
-else echo " ";echo "Did not find the host!"; echo Ending script; kill $pid
+then
+	echo " "
+	echo "Found host"
+	echo "Starting a port scan"
+	echo " "
+else 
+	echo " "
+	echo "Did not find the host!"
+       	echo Ending script
+	kill $pid
 fi
 
-rm ping_results.txt
+nmap -sV -v -sC -p 1-1000 $IP_ADDR | tee nmap_results.txt
 
-nmap -sV -v -sC -p 1-10000 $IP_ADDR | tee nmap_results.txt
+http=$( grep "http" -E nmap_results.txt -c )
+https=$( grep "https" -E nmap_results.txt -c )
+service=http
 
-echo "Complete!"
+# Use http by default
+if [ $http -gt 2 ]
+then
+	service=http
+elif [ $https -gt 2 ]
+then
+	service=https
+fi
+
+echo "Abridged results: "
+echo $( grep "/tcp " -E nmap_results.txt )
+echo $( grep "/udp" -E nmap_results.txt )
+
+if [ $https -gt 2 ] || [ $http -gt 2 ]
+then
+	webport=$( grep "http" -E nmap_results.txt | sed '2,2!d' | cut -d/ -f1 )	
+	echo " "
+	# Needs to account for multiple ports
+	echo "Found a webserver on port $webport."
+	gobuster dir --url $service://$IP_ADDR:$webport/ -x html,php,txt -w common.txt | tee gobuster_results.txt
+else
+	echo "No webserver found."
+fi
+
+echo "cat the text files created for the complete results."
+echo "Commands run: "
+echo "nmap -sV -v -sC -p 1-1000 $IP_ADDR"
+echo "gobuster dir --url http://$IP_ADDR:$webport/ -x html,php,txt -w common.txt"
+
+echo "If you didn't get the results you wanted, try either scanning different ports or using a different wordlist file on the web directory scan."
 
